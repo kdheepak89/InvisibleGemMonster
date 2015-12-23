47294cd516d14dd65986684d77e1b5e7cdbd3c51
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-12-23 07:08:03 -0700

    Update tumblr.py

--- tumblr.py
+++ tumblr.py
@@ -2,5 +2,6 @@ blog = [
         'gravi-teamfalls', # official gravity falls
         'kingofooo', # official adventure time
         'jasonfunderburker', # official over the garden wall
-        'stevencrewniverse' # official steven universe
+        'stevencrewniverse', # official steven universe
+        'laurenzuke' # Lauren Zuke is a storyboard artist on Steven Universe
         ]
