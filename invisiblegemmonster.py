d1d613a5ae8cd0f6aecf5426c0e55277b97bdc0c
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-12-23 21:32:34 -0700

    Fix select_flair

--- invisiblegemmonster.py
+++ invisiblegemmonster.py
@@ -182,7 +182,7 @@ class InvisibleGemMonster(object):
         if self.is_post_about('Steven Universe', tags):
             submission = self.submit_to('stevenuniverse', url, tags, post_title, post_time, blog)
             if submission is not None:
-                submission.select_flair(flair_template_id="crewniverse")
+                submission.select_flair(flair_template_id='95e7a5ee-7da7-11e5-ae5f-0e7e1b254bb1')
 
         if self.is_post_about('Undertale', tags):
             submission = self.submit_to('undertale', url, tags, post_title, post_time, blog)
