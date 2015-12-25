a9b47ebf24caa2ad0c8b5a435f46b310e77355c5
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-12-25 01:29:34 -0700

    Add comma

--- tumblr.py
+++ tumblr.py
@@ -4,5 +4,5 @@ blog = {
         'jasonfunderburker':True, # official over the garden wall
         'stevencrewniverse':True, # official steven universe
         'laurenzuke':True, # Lauren Zuke is a storyboard artist on Steven Universe
-        'rebeccasugar':True
+        'rebeccasugar':True,
         }
